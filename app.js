1c48171b88c3b60869c20ff4dc8c2603a51a4537
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-03-06 21:26:40 +0300

    go

--- app.js
+++ app.js
@@ -18,9 +18,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
 app.use('/', indexRouter);
-
-
 app.use('/etudiant', etudiantRouter);
 
 app.use(function(req, res, next) { next(createError(404)); });
