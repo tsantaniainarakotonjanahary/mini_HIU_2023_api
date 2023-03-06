c8a945eac20b3b1e954d71036ae6ffc4968d07f1
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-03-06 21:20:32 +0300

    go

---
+++ app.js
@@ -0,0 +1,39 @@
+var createError = require('http-errors');
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var indexRouter = require('./routes/index');
+var etudiantRouter = require('./routes/Etudiant');
+const cors = require('cors');
+var app = express();
+require('dotenv').config();
+
+const fileUpload = require('express-fileupload');
+
+app.use(cors());
+
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/', indexRouter);
+
+
+app.use('/etudiant', etudiantRouter);
+
+app.use(function(req, res, next) { next(createError(404)); });
+
+app.use(function(err, req, res, next) 
+{
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+var port = 3000;
+app.listen(port, () => console.log(` listening on port ${port}!`));
+
+module.exports = app;
---
+++ routes/Etudiant.js
@@ -0,0 +1,173 @@
+var express = require('express');
+var router = express.Router();
+const MongoClient = require('mongodb').MongoClient;
+const ObjectId = require("mongodb").ObjectId;
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+
+
+
+router.get('/', auth , function(req, res, next) { res.send('Etudiant'); });
+
+
+function auth(req, res, next) 
+{
+    const token = req.header('x-auth-token');
+    if (!token) 
+    {
+        return res.status(401).json({ message: 'Aucun token, autorisation refusée' });
+    }
+
+    try 
+    {
+        const decoded = jwt.verify(token, "Tsanta");
+        req.user = decoded;
+        next();
+    } 
+    catch (err) 
+    {
+        res.status(400).json({ message: 'Token non valide' });
+    }
+}
+
+
+router.post('/login', async (req, res) => {
+
+    const email = req.body.email;
+    const password = req.body.password;
+    const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
+    await client.connect();
+
+    const db = client.db("hiu");
+
+    let user = await db.collection("etudiant").findOne({ email: email });
+
+    if (!user) 
+    {
+        return res.status(401).json({ message: "Utilisateur non trouvé" });
+    }
+
+
+    const passwordIsValid = await bcrypt.compare(password, user.password);
+
+    if (!passwordIsValid) 
+    {
+        return res.status(401).json({ message: "Mot de passe incorrect" });
+    }
+
+    const token = jwt.sign({ id: user._id }, "Tsanta", { expiresIn: 86400 });
+
+    res.status(200).json({ user: user, token: token });
+
+    client.close();
+});
+
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+router.post('/register', async (req, res) => {
+
+    
+    const nom = req.body.nom;
+    const prenom = req.body.prenom;
+    const email = req.body.email;
+    const password = req.body.password;
+    const passwordConf = req.body.passwordConf;
+    const tel = req.body.tel;
+    const profil = req.body.profil;
+    const classe = req.body.classe;
+
+
+    if (password !== passwordConf) {
+        return res.status(400).json({ message: "Les mots de passe ne correspondent pas" });
+    }
+
+    if (!emailRegex.test(email)) {
+        return res.status(400).json({ message: "Adresse e-mail non valide" });
+    }
+
+    if (!/[A-Z]/.test(password)) {
+        return res.status(400).json({ message: "Le mot de passe doit contenir au moins une lettre majuscule" });
+    }
+
+    if (!/[a-z]/.test(password)) {
+        return res.status(400).json({ message: "Le mot de passe doit contenir au moins une lettre minuscule" });
+    }
+
+    if (!/[0-9]/.test(password)) {
+        return res.status(400).json({ message: "Le mot de passe doit contenir au moins un chiffre" });
+    }
+
+    if (!/[^A-Za-z0-9]/.test(password)) {
+        return res.status(400).json({ message: "Le mot de passe doit contenir au moins un caractère spécial" });
+    }
+
+    if (password.length < 8) {
+        return res.status(400).json({ message: "Le mot de passe doit contenir au moins 8 caractères" });
+    }
+
+        
+    const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
+    await client.connect();
+    const db = client.db("hiu");
+
+    const emailExists = await db.collection("etudiant").findOne({ email: email });
+
+    if (emailExists) 
+    {
+        return res.status(400).json({ message: "Cette adresse e-mail est déjà utilisée" });
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    const newEtudiant = {
+        nom: nom,
+        prenom: prenom,
+        email: email,
+        password: hash,
+        profil: profil,
+        classe : classe , 
+        tel : tel,
+    };
+
+    const { insertedId } = await db.collection("etudiant").insertOne(newEtudiant);
+
+    const token = jwt.sign({ id: insertedId }, "Tsanta", { expiresIn: 86400 });
+
+    res.status(200).json({ message: "Profile created successfully" , token  });
+    client.close();
+});
+
+router.put('/update', auth, async (req, res) => {
+    const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority',{ useUnifiedTopology: true });
+    await client.connect();
+    const db = client.db("hiu");
+    let user = await db.collection("etudiant").findOne({_id: new ObjectId(req.user.id)});
+    if (!user) 
+    {
+        return res.status(401).json({ message: "Utilisateur non trouvé" });
+    }
+    user.nom = req.body.nom || user.nom;
+    user.prenom = req.body.prenom || user.prenom;
+    user.email = req.body.email || user.email;
+    user.profil = req.body.profil || user.profil;
+    user.classe = req.body.classe || user.classe;
+    user.tel = req.body.tel|| user.tel;
+
+if(req.body.password){ user.password = await bcrypt.hash(req.body.password, 10); }
+db.collection("etudiant").updateOne({_id: new ObjectId(req.user.id)}, { $set: user },(err, result) => {
+    if (err) {
+        console.log(err);
+        return res.status(500).json({ message: "Error updating profile" });
+    }
+    client.close();
+    res.status(200).json({ message: "Profile updated successfully" });
+  });
+});
+
+
+
+
+module.exports = router;
+
