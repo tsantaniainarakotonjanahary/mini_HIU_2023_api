fb639d52fd92e94cee5463e83779cacc3cebea2f
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-03-07 07:40:36 +0300

    go

--- routes/Program.js
+++ routes/Program.js
@@ -82,6 +82,64 @@ router.delete('/', auth, async function(req, res, next) {
     client.close();
   });
 
+  router.put('/:idProgram', auth, async function(req, res, next) {
+    const etudiantId = req.user.id;
+    const idProgram = req.params.idProgram;
+    const numJour = req.body.numJour;
+    const heure_debut = req.body.heure_debut;
+    const heure_fin = req.body.heure_fin;
+    const matiere = req.body.matiere;
+    const theme = req.body.theme;
+  
+    const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
+    await client.connect();
+    const db = client.db("hiu");
+  
+    // Vérifier si le programme à mettre à jour existe et appartient à l'étudiant connecté
+    const existingProgram = await db.collection("program").findOne({ _id: new ObjectId(idProgram), etudiantId });
+    if (!existingProgram) {
+      res.status(404).json({ message: "Programme non trouvé ou n'appartient pas à l'étudiant connecté" });
+      client.close();
+      return;
+    }
+  
+    // Vérifier si le nouveau programme chevauche avec un programme existant autre que celui à mettre à jour
+    const chevauchement = await db.collection("program").findOne({
+      etudiantId,
+      numJour,
+      _id: { $ne: existingProgram._id },
+      $or: [
+        { heure_debut: { $lt: heure_debut } , heure_fin: { $gt: heure_debut } },
+        { heure_debut: { $gte: heure_debut, $lt: heure_fin } },
+        { heure_debut: { $lte: heure_debut } , heure_fin: { $gte: heure_fin } },
+        { heure_debut: { $lte: heure_debut } , heure_fin: { $gt: heure_debut, $lt: heure_fin } },
+        { heure_debut: { $gte: heure_debut, $lt: heure_fin }, heure_fin: { $gte: heure_fin } },
+      ]
+    });
+  
+    if (chevauchement) {
+      res.status(400).json({ message: "Le nouveau programme chevauche avec un programme existant" });
+      client.close();
+      return;
+    }
+  
+    // Mettre à jour le programme dans la base de données
+    const result = await db.collection("program").updateOne(
+      { _id: new ObjectId(idProgram), etudiantId },
+      { $set: { heure_debut: heure_debut, heure_fin: heure_fin, matiere, theme, numJour } }
+    );
+  
+    if (result.modifiedCount === 1) {
+      const updatedProgram = { _id: existingProgram._id, etudiantId, heure_debut: heure_debut, heure_fin: heure_fin, matiere, theme, numJour };
+      res.status(200).json({ program: updatedProgram, message: "Programme mis à jour dans l'emploi du temps" });
+    } else {
+      res.status(404).json({ message: "Programme non trouvé ou n'appartient pas à l'étudiant connecté" });
+    }
+  
+    client.close();
+  });
+  
+
   module.exports = router;
   
   
