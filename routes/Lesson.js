75fbab42719b19c7a28e12ecbf587c1cf1ff474c
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2023-03-07 19:44:12 +0300

    Clean code in Lesson Router

--- routes/Lesson.js
+++ routes/Lesson.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 const auth = require("../authentification/auth");
 const lessonService = require('../services/LessonService')
-const {scrapGoogleSearch} = require('../services/ScrapService')
 
 router.get("/", auth, (req, res) => {
   return res.json("LevelTest");
@@ -17,22 +16,7 @@ router.post("/generate", auth, async(req, res, next) => {
     }
     try{
         const generatedLessons = await lessonService.generateLessons(subject,theme)
-        const datas = generatedLessons.datas
-        if (!datas) return res.status(500).json({
-            error: "Erreur interne du serveur"
-        })
-        const formatedLessonsArray = []
-        for(let i=0;i<datas.length;i++){
-            const scrapedSearch = await scrapGoogleSearch(`${subject} ${theme} ${datas[i].toString().trim()}`)
-            formatedLessonsArray.push({
-                lessonTitle: datas[i].toString(),
-                suggestedWebsites: scrapedSearch
-            })
-        }
-        const newResponse = {
-            datas: formatedLessonsArray
-        }
-        res.send(newResponse)
+        res.send(generatedLessons)
     }catch(err){
         console.log(err)
         res.status(500).json({
--- services/LessonService.js
+++ services/LessonService.js
@@ -1,27 +1,47 @@
 const openai = require("../local-modules/openai/openaiLocal");
+const { scrapGoogleSearch } = require("../services/ScrapService");
 
-const generateLessons = async (examType, theme) => {
+const generateLessons = async (subject, theme) => {
   const prompt = `
     Sans mettre aucune introduction ni conclusion dans ta reponse, genere un fichier json bien formatté listant 
-    la liste des lessons que je dois maitriser pour mon examen de ${examType} sur le theme de ${theme} en suivant un ordre de priorité.
+    la liste des lessons que je dois maitriser pour mon examen de ${subject} sur le theme de ${theme} en suivant un ordre de priorité.
     Site les grands points. Au max 5 lessons.
     Le format devrait etre un peu comme suit:
     {
         datas:["lesson1","lesson2"]
     }
 `;
-  try{
+  try {
     const completions = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        max_tokens: 1000,
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 1000,
+    });
+
+    const lessonList = JSON.parse(completions.data.choices[0].text.trim());
+
+    const datas = lessonList.datas;
+    if (!datas)
+      return res.status(500).json({
+        error: "Erreur interne du serveur",
       });
-    
-      const lessonList = JSON.parse(completions.data.choices[0].text.trim())
-      return lessonList;
-  }catch(err){
-    console.log(err)
-    throw err
+    const formatedLessonsArray = [];
+    for (let i = 0; i < datas.length; i++) {
+      const scrapedSearch = await scrapGoogleSearch(
+        `${subject} ${theme} ${datas[i].toString().trim()}`
+      );
+      formatedLessonsArray.push({
+        lessonTitle: datas[i].toString(),
+        suggestedWebsites: scrapedSearch,
+      });
+    }
+    const newResponse = {
+      datas: formatedLessonsArray,
+    };
+    return newResponse;
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
 };
 
