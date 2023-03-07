f6ee2f74c72cbfe4131063ba2a2ff239e2112a70
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-03-07 20:08:22 +0300

    go

--- routes/Todo.js
+++ routes/Todo.js
@@ -5,6 +5,23 @@ const ObjectId = require("mongodb").ObjectId;
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+
+router.post('/', auth, async function(req, res, next) {
+    const etudiantId = req.user.id;
+    const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
+    await client.connect();
+    const db = client.db("hiu");
+    const newTodo = {
+        etudiantId,
+      date_today: new Date(req.body.date_today),
+      tache: req.body.tache,
+      isDone: req.body.isDone || "no"
+    };
+    const insertedTodo = await db.collection("todo").insertOne(newTodo);
+    res.status(201).json({ program: insertedTodo, message: "Todo ajouté " });
+    client.close();
+});
+
 function auth(req, res, next) 
 {
     const token = req.header('x-auth-token');
@@ -25,22 +42,6 @@ function auth(req, res, next)
     }
 }
 
-router.post('/', auth, async function(req, res, next) {
-    const etudiantId = req.user.id;
-    const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
-    await client.connect();
-    const db = client.db("hiu");
-    const newTodo = {
-        etudiantId,
-      date_today: new Date(req.body.date_today),
-      tache: req.body.tache,
-      isDone: req.body.isDone || "no"
-    };
-    const insertedTodo = await db.collection("todo").insertOne(newTodo);
-    res.status(201).json({ program: insertedTodo, message: "Todo ajouté " });
-    client.close();
-});
-
 router.get('/:date', auth, async function(req, res, next) {
     const etudiantId = req.user.id;
     const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
@@ -53,110 +54,53 @@ router.get('/:date', auth, async function(req, res, next) {
     client.close();
 });
   
+router.put('/finir/:id', auth, async function(req, res, next) {
+  const etudiantId = req.user.id;
+  const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
+  await client.connect();
+  const db = client.db("hiu");
+  const todoId = req.params.id;
+  const updatedTodo = {
+      isDone: "yes"
+  };
+  const result = await db.collection("todo").updateOne({ _id: ObjectId(todoId), etudiantId: etudiantId }, { $set: updatedTodo });
+  if (result.modifiedCount === 0) {
+      return res.status(404).json({ message: "Todo non trouvé" });
+  }
+  res.status(200).json({ message: "Todo achevé avec succès" });
+  client.close();
+});
 
-/*
-router.delete('/', auth, async function(req, res, next) {
-    const etudiantId = req.user.id;
-    const idProgram = req.body.idProgram;
-  
-    const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
-    await client.connect();
-    const db = client.db("hiu");
-  
-    const result = await db.collection("program").deleteOne({ _id: new ObjectId(idProgram), etudiantId });
-  
-    if (result.deletedCount === 1) {
-      res.status(200).json({ message: "Programme supprimé de l'emploi du temps" });
-    } else {
-      res.status(404).json({ message: "Programme non trouvé ou n'appartient pas à l'étudiant connecté" });
-    }
-  
-    client.close();
-  });
+router.put('/:id', auth, async function(req, res, next) {
+  const etudiantId = req.user.id;
+  const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
+  await client.connect();
+  const db = client.db("hiu");
+  const todoId = req.params.id;
+  const updatedTodo = {
+      tache: req.body.tache
+  };
+  const result = await db.collection("todo").updateOne({ _id: ObjectId(todoId), etudiantId: etudiantId }, { $set: updatedTodo });
+  if (result.modifiedCount === 0) {
+      return res.status(404).json({ message: "Todo non trouvé" });
+  }
+  res.status(200).json({ message: "Todo mis à jour avec succès" });
+  client.close();
+});
 
-  router.put('/:idProgram', auth, async function(req, res, next) {
-    const etudiantId = req.user.id;
-    const idProgram = req.params.idProgram;
-    const numJour = req.body.numJour;
-    const heure_debut = req.body.heure_debut;
-    const heure_fin = req.body.heure_fin;
-    const matiere = req.body.matiere;
-    const theme = req.body.theme;
-  
-    const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
-    await client.connect();
-    const db = client.db("hiu");
-  
-    // Vérifier si le programme à mettre à jour existe et appartient à l'étudiant connecté
-    const existingProgram = await db.collection("program").findOne({ _id: new ObjectId(idProgram), etudiantId });
-    if (!existingProgram) {
-      res.status(404).json({ message: "Programme non trouvé ou n'appartient pas à l'étudiant connecté" });
-      client.close();
-      return;
-    }
-  
-    // Vérifier si le nouveau programme chevauche avec un programme existant autre que celui à mettre à jour
-    const chevauchement = await db.collection("program").findOne({
-      etudiantId,
-      numJour,
-      _id: { $ne: existingProgram._id },
-      $or: [
-        { heure_debut: { $lt: heure_debut } , heure_fin: { $gt: heure_debut } },
-        { heure_debut: { $gte: heure_debut, $lt: heure_fin } },
-        { heure_debut: { $lte: heure_debut } , heure_fin: { $gte: heure_fin } },
-        { heure_debut: { $lte: heure_debut } , heure_fin: { $gt: heure_debut, $lt: heure_fin } },
-        { heure_debut: { $gte: heure_debut, $lt: heure_fin }, heure_fin: { $gte: heure_fin } },
-      ]
-    });
-  
-    if (chevauchement) {
-      res.status(400).json({ message: "Le nouveau programme chevauche avec un programme existant" });
-      client.close();
-      return;
-    }
-  
-    // Mettre à jour le programme dans la base de données
-    const result = await db.collection("program").updateOne(
-      { _id: new ObjectId(idProgram), etudiantId },
-      { $set: { heure_debut: heure_debut, heure_fin: heure_fin, matiere, theme, numJour } }
-    );
-  
-    if (result.modifiedCount === 1) {
-      const updatedProgram = { _id: existingProgram._id, etudiantId, heure_debut: heure_debut, heure_fin: heure_fin, matiere, theme, numJour };
-      res.status(200).json({ program: updatedProgram, message: "Programme mis à jour dans l'emploi du temps" });
-    } else {
-      res.status(404).json({ message: "Programme non trouvé ou n'appartient pas à l'étudiant connecté" });
-    }
-  
-    client.close();
-  });
-
-  router.get('/', auth, async function(req, res, next) {
-    const etudiantId = req.user.id;
-    const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
-    await client.connect();
-    const db = client.db("hiu");
-    
-    const programs = {
-      0: [],
-      1: [],
-      2: [],
-      3: [],
-      4: [],
-      5: [],
-      6: []
-    };
-  
-    const cursor = await db.collection("program").find({ etudiantId });
-  
-    await cursor.forEach(program => {
-      programs[program.numJour].push(program);
-    });
-  
-    res.status(200).json(programs);
-  });
-  
-  */
+router.delete('/:id', auth, async function(req, res, next) {
+  const etudiantId = req.user.id;
+  const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
+  await client.connect();
+  const db = client.db("hiu");
+  const todoId = req.params.id;
+  const result = await db.collection("todo").deleteOne({ _id: ObjectId(todoId), etudiantId: etudiantId });
+  if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "Todo non trouvé" });
+  }
+  res.status(200).json({ message: "Todo supprimé avec succès" });
+  client.close();
+});
 
   module.exports = router;
   
