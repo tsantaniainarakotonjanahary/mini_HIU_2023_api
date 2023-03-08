1ea4730128a4e285232d57b65bf469d0d6792510
Tsantaniaina Rakotonjanahary <tsantaniainarakotonjanahary@gmail.com>
2023-03-08 06:47:30 +0300

    go

--- routes/Exam.js
+++ routes/Exam.js
@@ -6,18 +6,26 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const auth = require('../authentification/auth')
 
+
+
 router.post('/', auth, async function(req, res, next) {
     const etudiantId = req.user.id;
     const client = new MongoClient('mongodb+srv://tsanta:ETU001146@cluster0.6oftdrm.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true });
     await client.connect();
     const db = client.db("hiu");
-
-    
+  
+    const newExam = {
+        etudiantId,
+      date_debut: req.body.date_debut,
+      date_fin: req.body.date_fin,
+      matière: req.body.matière,
+      thème: req.body.thème
+    };
   
     const insertedExam = await db.collection("exam").insertOne(newExam);
-    res.status(201).json({ program: insertedExam, message: "Exament marqué" });
+    res.status(201).json({ examen: insertedExam, message: "Examen ajouté" });
     client.close();
-});
+  });
 
 
 module.exports = router;
