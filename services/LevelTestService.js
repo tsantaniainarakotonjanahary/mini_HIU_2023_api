0511941ce2b90ee00df0178cfa4af12a0dd3c861
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2023-03-09 03:07:46 +0300

    Adding more and more instructions

--- services/LevelTestService.js
+++ services/LevelTestService.js
@@ -3,9 +3,8 @@ const openai = require("../local-modules/openai/openaiLocal");
 
 const generateLevelTest = async (subject, theme) => {
   const prompt = `
-    Sans mettre aucune introduction ni conclusion dans ta reponse,
-    genere un fichier json bien formatté listant une serie de 10 questions
-    reponses, tel un quizz concernant la matiere ${subject} sur le theme ${theme}.
+    Genere un fichier json bien formatté listant une serie de 10 questions avec ses reponses,
+    tel un quizz concernant la matiere ${subject} sur le theme ${theme}.
     Le format devrait etre comme suit :
     {
        datas:[
@@ -14,14 +13,13 @@ const generateLevelTest = async (subject, theme) => {
            choix: [
             "choix1", "choix2", "choix3", "choix4"
            ],
-           idReponse: id du choix exacte dans le tableau de choix (par exemple 0 pour choix1)
+           idReponse: indice de la bonne reponse dans le tableau choix
          },
-         etc...
        ]
     }
     Exactement 1 seul choix est correct dans le tableau de choix,
     et l'indice de son emplacement dans le tableau est figuré dans le champs idReponse
-    Par exemple , si lùobjet est :
+    Par exemple , si l'objet est :
     {
       datas:[
         {
@@ -33,11 +31,10 @@ const generateLevelTest = async (subject, theme) => {
         }
       ]
    }
-    Cela signifierai que choix3 soit la bonne reponse car dans le tableau choix , il a l'id 2, qui
+    Cela signifierai que "choix3" soit la bonne reponse car dans le tableau choix , il a l'indice 2, qui
     est ensuite mentionné dans le champs idReponse, qui est également à son tour 2
     Un seul choix doit etre la vrai reponse.
     Fait en sorte que la reponse soit reellement correct.
-    Fait en sorte que ce soit complet en ne depassant pas 2500 tokens
 `;
   try {
     const completions = await openai.createCompletion({
