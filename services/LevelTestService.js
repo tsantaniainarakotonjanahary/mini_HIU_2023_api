4f7036e943200641ec5c26b3ebe1eb5dba224e3f
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2023-03-09 03:01:52 +0300

    Adding some instructions

--- services/LevelTestService.js
+++ services/LevelTestService.js
@@ -19,24 +19,38 @@ const generateLevelTest = async (subject, theme) => {
          etc...
        ]
     }
-
-    Un seul choix doit etre la vrai reponse et l'index qu'il a dans le tableau doit etre
-    celui qui est inscrit dans le champs idReponse.
+    Exactement 1 seul choix est correct dans le tableau de choix,
+    et l'indice de son emplacement dans le tableau est figuré dans le champs idReponse
+    Par exemple , si lùobjet est :
+    {
+      datas:[
+        {
+          question: "la question est ici",
+          choix: [
+           "choix1", "choix2", "choix3", "choix4"
+          ],
+          idReponse: 2
+        }
+      ]
+   }
+    Cela signifierai que choix3 soit la bonne reponse car dans le tableau choix , il a l'id 2, qui
+    est ensuite mentionné dans le champs idReponse, qui est également à son tour 2
+    Un seul choix doit etre la vrai reponse.
     Fait en sorte que la reponse soit reellement correct.
     Fait en sorte que ce soit complet en ne depassant pas 2500 tokens
 `;
-  try{
+  try {
     const completions = await openai.createCompletion({
-        model: "text-davinci-003",
-        prompt: prompt,
-        max_tokens: 2500,
-      });
-    
-      const levelTest = JSON.parse(completions.data.choices[0].text)
-      return levelTest;
-  }catch(err){
-    console.log(err)
-    throw err
+      model: "text-davinci-003",
+      prompt: prompt,
+      max_tokens: 2500,
+    });
+
+    const levelTest = JSON.parse(completions.data.choices[0].text);
+    return levelTest;
+  } catch (err) {
+    console.log(err);
+    throw err;
   }
 };
 
