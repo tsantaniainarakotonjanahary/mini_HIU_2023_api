89fe61715a8c7b190e328ea24ff76bda81032fad
Cedric Rabarijohn <cedrictianarabarijohn@gmail.com>
2023-03-09 15:05:47 +0300

    Fix lesson suggestions

--- services/LessonService.js
+++ services/LessonService.js
@@ -5,40 +5,51 @@ const generateLessons = async (subject, theme) => {
   const prompt = `
     Sans mettre aucune introduction ni conclusion dans ta reponse, genere un fichier json bien formatté listant 
     la liste des lessons que je dois maitriser pour mon examen de ${subject} sur le theme de ${theme} en suivant un ordre de priorité.
-    Site les grands points. Au max 5 lessons.
+    Site les grands points. Au max 3 lessons. dans chaque lecons 2 sites suggérées chacuns.
     Le format devrait etre un peu comme suit:
     {
-        datas:["lesson1","lesson2"]
+        datas:[
+          {
+            "lessonTitle":"titre leçon",
+            "suggestedWebsites":[
+              {
+                title: "Titre du site",
+                link: "lien du site",
+                snippet : "Une portion de texte du site ..."
+              }
+            ]
+          }
+        ]
     }
 `;
   try {
     const completions = await openai.createCompletion({
       model: "text-davinci-003",
       prompt: prompt,
-      max_tokens: 1000,
+      max_tokens: 3000,
     });
 
     const lessonList = JSON.parse(completions.data.choices[0].text.trim());
 
-    const datas = lessonList.datas;
-    if (!datas)
-      return res.status(500).json({
-        error: "Erreur interne du serveur",
-      });
-    const formatedLessonsArray = [];
-    for (let i = 0; i < datas.length; i++) {
-      const scrapedSearch = await scrapGoogleSearch(
-        `${subject} ${theme} ${datas[i].toString().trim()}`
-      );
-      formatedLessonsArray.push({
-        lessonTitle: datas[i].toString(),
-        suggestedWebsites: scrapedSearch,
-      });
-    }
-    const newResponse = {
-      datas: formatedLessonsArray,
-    };
-    return newResponse;
+    // const datas = lessonList.datas;
+    // if (!datas)
+    //   return res.status(500).json({
+    //     error: "Erreur interne du serveur",
+    //   });
+    // const formatedLessonsArray = [];
+    // for (let i = 0; i < datas.length; i++) {
+    //   const scrapedSearch = await scrapGoogleSearch(
+    //     `${subject} ${theme} ${datas[i].toString().trim()}`
+    //   );
+    //   formatedLessonsArray.push({
+    //     lessonTitle: datas[i].toString(),
+    //     suggestedWebsites: scrapedSearch,
+    //   });
+    // }
+    // const newResponse = {
+    //   datas: formatedLessonsArray,
+    // };
+    return lessonList;
   } catch (err) {
     console.log(err);
     throw err;
